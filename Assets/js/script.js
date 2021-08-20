06aa524430937a3de345f7a27400e2fe7a223825
Bonnie Reed <bonniejeanreed@gmail.com>
2021-08-20 15:28:07 -0500

    added comments to code explaining each step

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assigning original variables.
 var generateBtn = document.querySelector("#generate");
 var upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
@@ -7,26 +7,27 @@ var numberChar = ['1234567890'];
 var password = "";
 var keyChar = "";
 
+ //Prompts user for use of uppercase letters in password.
   function hasUpper() {
     var upChars = window.confirm("Would you like upper case letters in your password?");
     return upChars;
   }
-  //Prompts user for use of lowercase letters in password
+  //Prompts user for use of lowercase letters in password.
   function hasLower() {
     var lowChars = window.confirm("Would you like lower case letters in your password?");
     return lowChars;
   }
-  //Prompts user for use of special characters in password
+  //Prompts user for use of special characters in password.
   function hasSpec() {
     var specChars = window.confirm("Would you like special characters in your password?");
     return specChars;
   }
-  //Prompts user for use of numbers in password
+  //Prompts user for use of numbers in password.
   function hasNum() {
     var numChars = window.confirm("Would you like numbers in your password?");
     return numChars;
   }
-
+// Generates the password based on user input from on screen prompts.
     function generatePassword(){
       //prompts user for password length.
   var passOptionLen = window.prompt("How many characters would you like your password length? (8\-128)");
@@ -34,29 +35,33 @@ var keyChar = "";
     alert ("You did not enter a valid number.");
     passOptionLen = window.prompt("How many characters would you like your password length? (8\-128)");
   }
+  // Creating variables for if statements to create randomized password based on user selections.
   var upChars = hasUpper();
   var lowChars = hasLower();
   var specChars = hasSpec();
   var numChars = hasNum();
   password = "";
-
+  //Chooses random character from the upperCaseChar string.
   if (upChars) {
     keyChar= keyChar + upperCaseChar;
     password = password + upperCaseChar[(Math.floor(Math.random()*upperCaseChar.length))];
   }
+  //Chooses random character from the lowerCaseChar string.
   if (lowChars) {
     keyChar= keyChar + lowerCaseChar;
     password = password + lowerCaseChar[(Math.floor(Math.random()*lowerCaseChar.length))];
   }
+  //Chooses random character from the specialChar string.
   if (specChars) {
     keyChar= keyChar + specialChar;
     password = password + specialChar[(Math.floor(Math.random()*specialChar.length))];
   }
+  //Chooses random character from the numberChar string.
   if (numChars) {
     keyChar= keyChar + numberChar;
     password = password + numberChar[(Math.floor(Math.random()*numberChar.length))];
   }
-
+    // runs through each of the listed if statements and increments until passOptionlen user is met.
   for (let index = password.length; index < passOptionLen; index++) {
     password = password + keyChar[(Math.floor(Math.random()*keyChar.length))];
     document.querySelector("#password").innerHTML = password;
@@ -64,7 +69,7 @@ var keyChar = "";
 
 }
 
-// Write password to the #password input
+// This function calls on the generate password function to start the password creation.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -72,5 +77,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// This button starts the user input prompts to create a password made to user specifications.
 generateBtn.addEventListener("click", writePassword);
